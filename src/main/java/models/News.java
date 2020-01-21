195cb7af0997007455c07608d986edf3edc567e2
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-21 08:21:55 +0300

    Generate getters and setters for News class

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -15,5 +15,43 @@ public class News {
         this.type = "General";
     }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
