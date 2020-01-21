0d7d1d1e5fb0398286918f39d31359fca0109799
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-21 09:31:56 +0300

    Passing tests for class User

---
+++ src/test/java/models/UserTest.java
@@ -0,0 +1,36 @@
+package models;
+
+//import dao.DatabaseRule;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("ConstantConditions")
+public class UserTest {
+
+//    @Rule
+//    public DatabaseRule databaseRule = new DatabaseRule();
+
+    private User newUser(){
+        return new User("Lil Holmes","Lead","Detective","Crime");
+    }
+
+    @Test
+    public void initializeCorrectly(){
+        User user = newUser();
+        assertTrue(user instanceof User);
+    }
+
+    @Test
+    public void getFunctionsWorkWell(){
+        User user = newUser();
+        assertEquals(user.getName(),newUser().getName());
+        assertEquals(user.getPosition(),newUser().getPosition());
+        assertEquals(user.getRole(),newUser().getRole());
+        assertEquals(user.getDepartment(),newUser().getDepartment());
+    }
+
+
+}
