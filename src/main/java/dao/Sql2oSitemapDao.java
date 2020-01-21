5458aff615f572ad36088209c8837982c62aec58
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-21 11:34:50 +0300

    Create a Sql2o sitemap class

---
+++ src/main/java/dao/Sql2oSitemapDao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import models.Sitemap;
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class Sql2oSitemapDao {
+
+    public Sql2oSitemapDao() {
+    }
+
+    public List<Sitemap> allPaths() {
+        String sql = "SELECT * from sitemap;";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .executeAndFetch(Sitemap.class);
+        }
+    }
+}
