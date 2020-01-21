7754793af279990de5b1e5ae309d5e13052b5498
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-21 08:10:01 +0300

    Generate getters and setters for the fields in Departments class

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -12,4 +12,35 @@ public class Department {
         this.totalEmployees = 0;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTotalEmployees() {
+        return totalEmployees;
+    }
+
+    public void setTotalEmployees(int totalEmployees) {
+        this.totalEmployees = totalEmployees;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
