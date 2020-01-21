724cac2b681ad23998c7e7de6185e051338c9b7d
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-21 08:09:12 +0300

    Create POJO for Department class

---
+++ src/main/java/models/Department.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Department {
+    private String name;
+    private String description;
+    private int totalEmployees;
+    private int id;
+
+    public Department(String name, String description, int totalEmployees) {
+        this.name = name;
+        this.description = description;
+        this.totalEmployees = 0;
+    }
+
+}
