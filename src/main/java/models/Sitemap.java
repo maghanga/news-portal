30725ec115a1a6a0d6c7fe16753e68a143f49691
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-21 11:32:17 +0300

    Create Sitemap class, add POJO and generate getters and setters for respective fields

---
+++ src/main/java/models/Sitemap.java
@@ -0,0 +1,24 @@
+package models;
+
+public class Sitemap {
+    private String describe;
+    private String path;
+    private String id;
+
+    public Sitemap(String describe, String path) {
+        this.describe = describe;
+        this.path = path;
+    }
+
+    public String getDescr() {
+        return describe;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
