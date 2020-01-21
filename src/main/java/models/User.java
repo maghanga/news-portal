3f0b590c2349830b22adc90195fcd2a177331f0a
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-21 08:24:35 +0300

    Add POJO for the User class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,18 @@
 package models;
 
+import java.util.Objects;
+
 public class User {
+    private String name;
+    private String position;
+    private String role;
+    private String department;
+    private int id;
+
+    public User(String name, String position, String role, String department) {
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.department = department;
+    }
 }
