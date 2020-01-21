f225218253fce8d5d5134418d5a50c9a569305b5
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-21 08:25:14 +0300

    Generate getters dna setters for the fields in User class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -15,4 +15,44 @@ public class User {
         this.role = role;
         this.department = department;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
