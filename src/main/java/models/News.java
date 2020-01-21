15ff77d216f0b977b16b3e1440893921d15b5826
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-21 08:21:10 +0300

    Add POJO for News class

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,19 @@
 package models;
 
+import java.util.Objects;
+
 public class News {
+    private String title;
+    private String description;
+    private String type;
+    private String author;
+    private int id;
+
+    public News(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.type = "General";
+    }
+
+
 }
